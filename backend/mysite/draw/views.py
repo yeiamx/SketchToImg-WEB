8c156ccf3304ca28e1faa126a751f697d762c0d2
yeiamx <925166430@qq.com>
2018-04-06 20:19:20 +0800

    get the numpy version

--- backend/mysite/draw/views.py
+++ backend/mysite/draw/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from . import models
+import io
+from PIL import Image
+import numpy as np
 import base64
 
 @csrf_exempt
@@ -12,9 +15,14 @@ def index(request):
         #print(request.POST.get('data'))
 
         dataStr = str(request.POST.get('data'))
-        print(dataStr)
+        #print(dataStr)
         print('processing...')
         ori_image_data = base64.b64decode(dataStr)
+        image_bytes = io.BytesIO(ori_image_data)
+        im = Image.open(image_bytes)
+        arr = np.array(im)[:,:,0]
+        print(arr.shape)
+
         print('saving...')
         fout = open('d:\\test_draw.png', 'wb')
         fout.write(ori_image_data)
